a308c053cdcbffd86b97b59e43c8d7a4654ca726
CD <48509493+caidix@users.noreply.github.com>
2020-09-19 14:36:27 +0800

    :art:增加ajax错误上报

--- src/error/ajax-error.ts
+++ src/error/ajax-error.ts
@@ -1,6 +1,6 @@
-import ErrorMonitor from '../error/error-monitor';
-import { ErrorEnums, AlertEnums } from '../enums/error-enums';
-import { errorOptionTypes } from '../types/index';
+import ErrorMonitor from "../error/error-monitor";
+import { ErrorEnums, AlertEnums } from "../enums/error-enums";
+import { errorOptionTypes } from "../types/index";
 /**
  * Ajax的监听主要是为了发现服务接口返回值的问题
  * abort：外部资源中止加载时（比如用户取消）触发。如果发生错误导致中止，不会触发该事件。
@@ -13,39 +13,61 @@ import { errorOptionTypes } from '../types/index';
  */
 class AjaxError extends ErrorMonitor {
   public constructor(options: errorOptionTypes) {
-    super(options)
+    super(options);
   }
   // 重写Send方法
   public handleError() {
     if (!window || !window.XMLHttpRequest) return;
     const XmlSend = window.XMLHttpRequest.prototype.send;
-    /**
-     * currentTarget 返回当前触发事件的元素/ target 返回触发事件触发的源头元素
-     */
-    const _handleFunc = (e: ProgressEvent) => {
+    const XmlOpen = window.XMLHttpRequest.prototype.open;
+    const _handleFunc = (e: ProgressEvent | Event) => {
+      /**
+       * currentTarget 返回当前触发事件的元素/ target 返回触发事件触发的源头元素
+       */
       const target: any = e.target;
       if (target && target.status !== 200) {
-        console.log('ajax出错:', target)
         this.errorType = ErrorEnums.AJAX_ERROR; //错误类型
         this.errorAlert = AlertEnums.WARN; //错误等级
         this.msg = target.response;
         this.url = target.responseURL;
+        const cdreport = target.cdreport || {};
         this.errorStack = {
           status: target.status,
           statusText: target.statusText,
           eventType: e.type,
-          // method: target.fundebugTemp.
-        }
+          timeStamp: e.timeStamp,
+          ...cdreport,
+        };
+        this.handleReportError();
       }
-    }
+    };
+    XMLHttpRequest.prototype.open = function (...argument: any) {
+      const [method, rootUrl] = argument;
+      const _self: any = this;
+      _self["cdreport"] = {
+        method,
+        rootUrl,
+      };
+      return XmlOpen.apply(this, argument);
+    };
     XMLHttpRequest.prototype.send = function (...argument: any) {
-      console.log(argument)
-      this.addEventListener('error', _handleFunc, false);
-      this.addEventListener('load', _handleFunc, false);
-      this.addEventListener('abort', _handleFunc, false);
-      this.addEventListener('timeout', _handleFunc, false);
+      if (this.addEventListener) {
+        this.addEventListener("error", _handleFunc, false);
+        this.addEventListener("load", _handleFunc, false);
+        this.addEventListener("abort", _handleFunc, false);
+        this.addEventListener("timeout", _handleFunc, false);
+      } else if (this.onreadystatechange) {
+        // 重写onreadystatechange
+        const tempStateChange = this.onreadystatechange;
+        this.onreadystatechange = function (e) {
+          tempStateChange.apply(this, argument);
+          if (this.readyState === 4) {
+            _handleFunc(e);
+          }
+        };
+      }
       return XmlSend.apply(this, argument);
-    }
+    };
   }
 }
 export default AjaxError;
